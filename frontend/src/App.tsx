cf95ad0caa797db10f493ed70268a4cb7eb15b6b
SKREAMMY <kiran.pillai199@gmail.com>
2026-06-04 19:36:46 +0100

    Feaat: Created header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,10 +5,13 @@ import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
+import Header from "./components/Header";
 
 function App() {
   return (
     <BrowserRouter>
+      {/* header */}
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/sign-in" element={<SignIn />} />
