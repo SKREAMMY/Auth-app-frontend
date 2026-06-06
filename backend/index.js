5070b00b805a4ffb7be96a704fb6cedc5d05ba40
SKREAMMY <kiran.pillai199@gmail.com>
2026-06-06 13:42:56 +0100

    Feat: connection establishment to mongodb

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,22 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log("Connected to MongoDB");
+}).catch((err) => {
+    console.log("error connecting mongodb ", err);
+})
+
 
 const app = express();
+
+
+app.get('/', (req, res) => {
+    res.send("Hello World");
+});
 
 app.listen(3000, () => {
     console.log("server listening at port 3000");
