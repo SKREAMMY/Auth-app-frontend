c24eee67655216eda526ceceec582137556255bf
SKREAMMY <kiran.pillai199@gmail.com>
2026-06-06 15:52:37 +0100

    feat: added middleware and errorhandler

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const signUp = async (req, res) => {
+export const signUp = async (req, res, next) => {
     const { userName, email, password } = req.body;
     const hashedPassword = bcryptjs.hashSync(password, 10);
     console.log(userName, email, password);
@@ -13,9 +14,7 @@ export const signUp = async (req, res) => {
             message: "User created successfully"
         });
     } catch (err) {
-        res.status(500).json({
-            message: "User already exists"
-        })
+        next(errorHandler(500, err.message));
     }
 
 };
