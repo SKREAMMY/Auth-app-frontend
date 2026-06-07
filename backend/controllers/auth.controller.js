e6cd5a6a39b8dc78bd882f88b11a27ec614758f6
SKREAMMY <kiran.pillai199@gmail.com>
2026-06-07 15:26:25 +0100

    Feat: created signup and sign in with jwt access token

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
 
 export const signUp = async (req, res, next) => {
     const { userName, email, password } = req.body;
@@ -11,6 +12,8 @@ export const signUp = async (req, res, next) => {
         await newUser.save();
 
         res.status(201).json({
+            statusCode: 201,
+            success: true,
             message: "User created successfully"
         });
     } catch (err) {
@@ -18,3 +21,28 @@ export const signUp = async (req, res, next) => {
     }
 
 };
+
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+    console.log("email and password ", email, password);
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) {
+            return next(errorHandler(404, "Invalid Credentials"));
+        }
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) {
+            return next(errorHandler(401, "Wrong Credentials"));
+        }
+
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        console.log("token ", token);
+        const { password: hashedPassword, ...rest } = validUser._doc;
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+
+    } catch (err) {
+        next(err);
+    }
+}
