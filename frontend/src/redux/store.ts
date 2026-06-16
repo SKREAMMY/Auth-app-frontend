d5d5b33565093c72dfacc49bf468a260a4364473
SKREAMMY <kiran.pillai199@gmail.com>
2026-06-16 18:48:23 +0100

    feat: added redux persist

--- frontend/src/redux/store.ts
+++ frontend/src/redux/store.ts
@@ -1,12 +1,37 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
 
 import userReducer from "./user/userSlice";
 
-export const store = configureStore({
-  reducer: { user: userReducer },
+const rootReducer = combineReducers({
+  user: userReducer,
 });
 
-// Infer the `RootState` and `AppDispatch` types from the store itself
+import { persistReducer, persistStore } from "redux-persist";
+
+const storage = {
+  getItem: (key: string) => Promise.resolve(localStorage.getItem(key)),
+  setItem: (key: string, value: string) =>
+    Promise.resolve(localStorage.setItem(key, value)),
+  removeItem: (key: string) => Promise.resolve(localStorage.removeItem(key)),
+};
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
 export type RootState = ReturnType<typeof store.getState>;
-// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
 export type AppDispatch = typeof store.dispatch;
+
+export const persistor = persistStore(store);
